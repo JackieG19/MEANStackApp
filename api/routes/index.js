c5e0005775be598f72faeaf31807f58f1a9ce556
Jackie <jacgut19@gmail.com>
2018-03-31 01:32:26 +0000

    section 5 complete

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,30 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 var ctrlHotels = require('../controllers/hotels.controllers.js');
 var ctrlReviews = require('../controllers/reviews.controllers.js');
+var ctrlUsers = require('../controllers/users.controllers.js');
 
-// Hotel routes
+//Hotel Routes
 router
-  .route('/hotels')
-  .get(ctrlHotels.hotelsGetAll)
-  .post(ctrlHotels.hotelsAddOne);
-
-router
-  .route('/hotels/:hotelId')
-  .get(ctrlHotels.hotelsGetOne)
-  .put(ctrlHotels.hotelsUpdateOne);
-
-
-// Review routes
-router
-  .route('/hotels/:hotelId/reviews')
-  .get(ctrlReviews.reviewsGetAll)
-  .post(ctrlReviews.reviewsAddOne);
+    .route('/hotels')
+    .get(ctrlHotels.hotelsGetAll)
+    .post(ctrlHotels.hotelsAddOne);
 
 router
-  .route('/hotels/:hotelId/reviews/:reviewId')
-  .get(ctrlReviews.reviewsGetOne)
-  .put(ctrlReviews.reviewsUpdateOne);
+    .route('/hotels/:hotelId')
+    .get(ctrlHotels.hotelsGetOne)
+    .put(ctrlHotels.hotelsUpdateOne)
+    .delete(ctrlHotels.hotelsDeleteOne);
+    
+//Review Routes
+router
+    .route('/hotels/:hotelId/reviews')
+    .get(ctrlReviews.reviewsGetAll)
+    .post(ctrlUsers.authenticate, ctrlReviews.reviewsAddOne);
+    
+router
+    .route('/hotels/:hotelId/reviews/:reviewId')
+    .get(ctrlReviews.reviewsGetOne)
+    .put(ctrlReviews.reviewsUpdateOne)
+    .delete(ctrlReviews.reviewsDeleteOne);
+    
+// Authentication
+router
+    .route('/users/register')
+    .post(ctrlUsers.register);
+    
+router
+    .route('/users/login')
+    .post(ctrlUsers.login);
 
 module.exports = router;
